f488b334bbbb38509a9b2c1b821e490a57ba2723
owopqowo <owo9370@gmail.com>
2023-01-03 16:56:49 +0900

    카드 셔플 추가

--- README.md
+++ README.md
@@ -31,3 +31,29 @@ y축 : top, bottom, center의 키워드 및 %, 단위 사용 가능
 ### 문법
 flat : 자식 요소를 2D의 2차원에서 부모 요소와 동일한 평면에 배치  
 preserve-3d : 자식 요소를 3D 공간에 배치
+## JS DOM 탐색
+DOM에 수행하는 모든 연산은 document 객체에서 시작  
+DOM에서 null 값은 '존재하지 않음’이나 '해당하는 노드가 없음’을 의미
+![dom-links](https://user-images.githubusercontent.com/54467533/210304102-e125cb7c-21a4-4d05-b8bb-caf16d16e3d2.svg)
+### 트리 상단
+- <html> = document.documentElement
+- <body> = document.body
+- <head> = document.head
+### 자식 노드
+- childNodes : 텍스트 노드를 포함한 모든 자식 노드
+- firstChild : 첫 번째 자식 노드
+- lastChild : 마지막 자식 노드
+※ 자식 노드의 존재 여부를 검사할 땐 함수 elem.hasChildNodes()를 사용
+### 형제 노드
+- previousSibling : 이전 형제 노드
+- nextSibling : 다음 형제 노드
+### 부모 노드
+- parentNode : 부모 노드
+### 요소 간 이동
+![dom-links-elements](https://user-images.githubusercontent.com/54467533/210304129-923a6a57-73ff-42ab-8d84-691a4c7c31c7.svg)
+- children : 자식 노드 중 요소 노드
+- firstElementChild : 첫 번째 자식 요소 노드
+- lastElementChild : 마지막 자식 요소 노드
+- previousElementSibling : 이전 형제 요소 노드
+- nextElementSibling : 다음 형제 요소 노드
+- parentElement : 부모 요소 노드
--- index.js
+++ index.js
@@ -18,12 +18,129 @@ const cardObj = [
 ];
 
 const cardContainerElem = document.querySelector('.card-container');
+const playGameButtonElem = document.querySelector('#playGame');
+let cards = [];
+const gridAreaTemplate = '';
+const numCards = cardObj.length;
+let cardPositions = [];
+const areas = '"a a" "a a"';
 
-createCards();
+loadGame();
+
+function loadGame() {
+  createCards();
+
+  cards = document.querySelectorAll('.card');
+
+  playGameButtonElem.addEventListener('click', () => {
+    startGame();
+  });
+}
+
+function startGame() {
+  initNewGame();
+  startRound();
+}
+
+function initNewGame() {}
+
+function startRound() {
+  initNewRound();
+  transformGridArea(areas);
+  flipCards(true);
+  shuffleCards();
+}
+
+function initNewRound() {}
+
+function transformGridArea(areas) {
+  cardContainerElem.style.gridTemplateAreas = areas;
+}
+
+function flipCard(card, flipToBack) {
+  const innerCardElem = card.firstElementChild;
+
+  if (flipToBack && !innerCardElem.classList.contains('card--flip')) {
+    innerCardElem.classList.add('card--flip');
+  } else if (innerCardElem.classList.contains('card--flip')) {
+    innerCardElem.classList.remove('card--flip');
+  }
+}
+
+function flipCards(flipToBack) {
+  cards.forEach((card, index) => {
+    setTimeout(() => {
+      flipCard(card, flipToBack);
+    }, index * 100);
+  });
+}
+
+function shuffleCards() {
+  const id = setInterval(shuffle, 12);
+  let shuffleCount = 0;
+  function shuffle() {
+    randomizeCardPositions();
+    if (shuffleCount === 500) {
+      clearInterval(id);
+      dealCards();
+    } else {
+      shuffleCount++;
+    }
+  }
+}
+
+function randomizeCardPositions() {
+  const random1 = Math.floor(Math.random() * numCards) + 1;
+  const random2 = Math.floor(Math.random() * numCards) + 1;
+
+  const temp = cardPositions[random1 - 1];
+  cardPositions[random1 - 1] = cardPositions[random2 - 1];
+  cardPositions[random2 - 1] = temp;
+}
+
+function dealCards() {
+  addCardsToAppropriateCell();
+  const areasTemp = returnGridAreasMappedToCardPos();
+  transformGridArea(areasTemp);
+}
+
+function returnGridAreasMappedToCardPos() {
+  let firstPart = '';
+  let secondPart = '';
+  let areas = '';
+
+  cards.forEach((_, index) => {
+    if (cardPositions[index] == 1) {
+      areas = areas + 'a ';
+    } else if (cardPositions[index] == 2) {
+      areas = areas + 'b ';
+    } else if (cardPositions[index] == 3) {
+      areas = areas + 'c ';
+    } else if (cardPositions[index] == 4) {
+      areas = areas + 'd ';
+    }
+    if (index == 1) {
+      firstPart = areas.substring(0, areas.length - 1);
+      areas = '';
+    } else if (index == 3) {
+      secondPart = areas.substring(0, areas.length - 1);
+    }
+  });
+
+  console.log(`"${firstPart}" "${secondPart}"`);
+
+  return `"${firstPart}" "${secondPart}"`;
+}
+
+function addCardsToAppropriateCell() {
+  cards.forEach((card) => {
+    addCardToContainer(card);
+  });
+}
 
 function createCards() {
   cardObj.forEach((cardItem) => {
-    cardContainerElem.appendChild(createCard(cardItem));
+    createCard(cardItem);
   });
 }
 
@@ -37,9 +154,18 @@ function createCard(cardItem) {
       <img src="${cardItem.imagePath}" alt="" class="card__image">
     </div>
     <div class="card__back">
-      <img src="${cardItem.imagePath}" alt="" class="card__image">
+      <img src="./images/card-back-Blue.png" alt="" class="card__image">
     </div>
   </div>
   `;
-  return cardElem;
+  addCardToContainer(cardElem);
+  initCardPositions(cardElem);
+}
+
+function initCardPositions(card) {
+  cardPositions.push(card.id);
+}
+
+function addCardToContainer(card) {
+  cardContainerElem.appendChild(card);
 }
